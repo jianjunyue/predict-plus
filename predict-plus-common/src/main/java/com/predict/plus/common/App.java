a8837eb1c99e3caa1b9aa9815a89970644a4874e
jianjunyue <409494312@qq.com>
2021-02-23 11:10:01 +0800

    first commit

---
+++ predict-plus-common/src/main/java/com/predict/plus/common/App.java
@@ -0,0 +1,13 @@
+package com.predict.plus.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ predict-plus-facade/src/main/java/com/predict/plus/facade/App.java
@@ -0,0 +1,13 @@
+package com.predict.plus.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
